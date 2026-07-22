51722e0888ac44de2cc272e683b57355289d6fb0
Chetan Mhavale <cbmhavale21@gmail.com>
2026-07-22 09:41:23 +0530

    Add files via upload
    this program logic is calculate area of rectangle effectively.

---
+++ areacalcu.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Rectangle Area Calculator :"<<endl;
+    cout<<"Enter length :";
+    cin>>a; 
+    cout<<"Enter breadth :";
+    cin>>b;
+    cout<<"your area of rectangle is :"<<a*b;
+    return 0;
+}
