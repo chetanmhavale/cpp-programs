de2beceaeb7bd449a9981029562c6e75f58792b9
Chetan Mhavale <cbmhavale21@gmail.com>
2026-07-29 10:06:39 +0530

    Add files via upload

---
+++ reversenum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+int rev=0;
+int c=1;
+cout<<"Enter your number convert to Reverse"<<endl;
+cin>>a;
+while(a>0){
+b=a%10;
+rev=b+(rev*c);
+a/=10;
+c=10;
+}
+cout<<"your reverse number:"<<rev;
+return 0;
+}
+
+
+
+
